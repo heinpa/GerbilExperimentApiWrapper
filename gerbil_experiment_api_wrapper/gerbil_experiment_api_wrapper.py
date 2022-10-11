d16b88bb11137745dcf428d3bc1c5e007d4f1aa4
heinpa <heipa@posteo.de>
2022-10-11 17:20:37 +0200

    add tests for wrapper api

--- gerbil_experiment_api_wrapper/gerbil_experiment_api_wrapper.py
+++ gerbil_experiment_api_wrapper/gerbil_experiment_api_wrapper.py
@@ -3,11 +3,8 @@ import requests
 import time
 from bs4 import BeautifulSoup
 import validators
+from pathlib import Path
 
-
-# better selection of live/local model 
-# better more dynamic input options for init
-# more output
 
 class GerbilExperimentApiWrapper:
 
@@ -27,14 +24,29 @@ class GerbilExperimentApiWrapper:
     upload_data_postfix = """experimentData={{"type":"QA","matching":"STRONG_ENTITY_MATCH","annotator":[{annotator}],"dataset":["{dataset}"],"answerFiles":[{answerFiles}],"questionLanguage":"{questionLanguage}"}}"""
 
 
-    def __init__(self, **kwargs):
-        # TODO: documentation!!!
+    def __init__(self, language, gold_standard_file, **kwargs):
+        """
+        Parameters
+        ---------
+        gold_standard_file : str, required
+            The path to a gold standard dataset
+        test_results_file : str, optional
+            The path to a test result dataset to be used *instead* of a live annotator
+        live_annotator_name : str, optional
+            The name of a live annotator to be used *instead* of local test results; 
+            requires a value for 'live_annotator_url'
+        live_annotator_url : str, optional
+            The URL for a live annotator; requires a value for 'live_annotator_name'
+        language : str, required
+            The question language
+        """
+
         # gold standard file and language always needs to be defined
-        self.gold_standard_file = kwargs.get('gold_standard_file')
+        self.gold_standard_file = gold_standard_file
+        self.language = language
         self.test_results_file = kwargs.get('test_results_file')
         self.live_annotator_name = kwargs.get('live_annotator_name')
         self.live_annotator_url = kwargs.get('live_annotator_url')
-        self.language = kwargs.get('language')
 
         # upload files / set live annotator (prefer local files)
         self.upload_file(self.gold_standard_file, self.gold_standard_name, 'application/json')
@@ -53,15 +65,16 @@ class GerbilExperimentApiWrapper:
                             + "Missing results file or live annotator.")
 
         # run experiment with set configuration
-        self.experiment_id = self.upload_experment_configuration()
-        if self.result_url_valid(self.get_results_url()):
+        self.experiment_id = self.upload_experiment_configuration()
+        if self.is_url_valid(self.get_results_url()):
             print(f"initialized GerbilExperimentApiWrapper with experiment: {self.get_experiment_url}{self.experiment_id}")
         else:
             raise Exception(f"Could not initialize GerbilBenchmarkService!"
                             + "The experiment did not return valid results.")
         
 
-    def result_url_valid(self, results_url):
+    def is_url_valid(self, results_url):
+        # basic validation of the result url
         if validators.url(results_url):
             return True
         else:
@@ -69,11 +82,13 @@ class GerbilExperimentApiWrapper:
 
 
     def set_live_annotator(self, name, url):
-        # TODO: validate url
-        self.annoator = f"{self.live_annotator_prefix}_{name}({url})"
+        if self.is_url_valid(url):
+            self.annoator = f"{self.live_annotator_prefix}_{name}({url})"
+        else: 
+            raise Exception(f"Invalid annotator URL for annotator {name}: {url}")
 
         
-    def upload_experment_configuration(self):
+    def upload_experiment_configuration(self):
 
         execute_url = self.upload_configuration_url + self.upload_data_postfix.format(
             dataset = f"{self.uploaded_dataset_prefix}_{self.gold_standard_name}({self.gold_standard_file})",
@@ -83,16 +98,15 @@ class GerbilExperimentApiWrapper:
             questionLanguage = self.language
         )
 
-        print(execute_url)
-
         cnt = 0
         while cnt < 5:
             try:
-                # if no other experiment is running
+                # check that no other experiment is running
                 if requests.get(self.check_running_url).text == '':
+                    # execute reauest with experiment configuration
                     response = requests.get(execute_url)
                     if response.status_code == 200:
-                        return response.text
+                        return response.text # return the experiment id 
                 else:
                     print("another experiment is running, waiting for 60 seconds ...")
                     cnt += 1 
@@ -109,6 +123,7 @@ class GerbilExperimentApiWrapper:
 
     def get_results(self):
         query_url = self.get_results_url()
+        # extract json-ld from response content
         soup = BeautifulSoup(requests.get(query_url).text, "html.parser")
         data = [
             json.loads(x.string) for x in soup.find_all("script", type="application/ld+json")
@@ -116,25 +131,37 @@ class GerbilExperimentApiWrapper:
         return data[0]
         
 
-    def read_file_bytes(self, file):
-        return open(file, 'rb')
-
-
     def upload_file(self, file, name, type, multiselect=None):
+        """Upload gold starndard or test results file to be used in the experiment
+
+        Parameters
+        ----------
+        file : str, required
+            The file path 
+        name : str, required
+            The name to be used in the experiment configuration
+        type : str, required
+            The application type for the file
+        multiselect : bool, optional
+            The path to the referenced gold standard file when uploading test results
+        """
+
         if len(file) > 100:
             raise Exception("File names must not exceed 100 characters!")
+        # TODO: figure out how upload handles file paths
+        # currently the server responds with 500, seeminly because it's out of space
         files = {
-            "file": (file, self.read_file_bytes(file), type)
+            "file": (file, open(file, 'rb'), type)
         }
         data = {
             'name': name,
             'multiselect': multiselect 
         }
-        request = requests.post(self.file_upload_url, data=data, files=files)
-        response = request.json()
-        if request.status_code == 200:
-            return response
-        else: 
-            raise Exception(f"file upload not successful: {request.status_code}: {request.content}")
-
-
+        try:
+            request = requests.post(self.file_upload_url, data=data, files=files)
+            response = request.json()
+            if request.status_code != 200:
+                raise Exception(f"file upload not successful: {request.status_code}: {request.content}")
+            else: return response
+        except Exception as e:
+            raise Exception(f"file upload not successful: {e}")
---
+++ tests/test_wrapper_helper_functions.py
@@ -0,0 +1,67 @@
+from gerbil_experiment_api_wrapper.gerbil_experiment_api_wrapper import GerbilExperimentApiWrapper
+from unittest.mock import patch
+import pytest
+
+
+# TODO: live API tests
+# TODO: mock return of API calls (result url, id)
+# TODO: check if patches can be done with setUp or fixtures
+# TODO: verify upload_file() params and final experiment_data
+
+gold_standard_file = "./tests/results_gold.json"
+test_results_file = "./tests/result_test.json"
+language = "en"
+live_annotator_name = "QAnswer"
+live_annotator_url = "http://porque.cs.upb.de:40123/qanswer/gerbil"
+
+
+@patch('gerbil_experiment_api_wrapper.gerbil_experiment_api_wrapper.GerbilExperimentApiWrapper.upload_experiment_configuration')
+@patch('gerbil_experiment_api_wrapper.gerbil_experiment_api_wrapper.GerbilExperimentApiWrapper.upload_file')
+@patch('gerbil_experiment_api_wrapper.gerbil_experiment_api_wrapper.GerbilExperimentApiWrapper.is_url_valid')
+def test_initialize_wrapper_with_local_files(mock_upload_experiment_configuration, mock_upload_file, mock_is_url_valid):
+    try:
+        wrapper = GerbilExperimentApiWrapper(
+            gold_standard_file=gold_standard_file, language=language,
+            test_results_file=test_results_file
+        )
+        assert wrapper.use_live_annotator == False
+        assert mock_upload_file.called
+        # assert values are stored in wrapper object
+        assert wrapper.gold_standard_file == gold_standard_file
+        assert wrapper.test_results_file == test_results_file
+        assert wrapper.language == language
+        # assert the configuration upload function was called
+        assert mock_upload_experiment_configuration.called
+        assert mock_is_url_valid.called
+    except Exception:
+        assert False
+
+
+@patch('gerbil_experiment_api_wrapper.gerbil_experiment_api_wrapper.GerbilExperimentApiWrapper.upload_experiment_configuration')
+@patch('gerbil_experiment_api_wrapper.gerbil_experiment_api_wrapper.GerbilExperimentApiWrapper.upload_file')
+@patch('gerbil_experiment_api_wrapper.gerbil_experiment_api_wrapper.GerbilExperimentApiWrapper.is_url_valid')
+def test_initialize_wrapper_with_live_annotator(mock_upload_experiment_configuration, mock_upload_file, mock_is_url_valid):
+    try:
+        wrapper = GerbilExperimentApiWrapper(
+            gold_standard_file=gold_standard_file, language=language,
+            live_annotator_name=live_annotator_name,
+            live_annotator_url=live_annotator_url
+        )
+        assert wrapper.use_live_annotator == True
+        assert mock_upload_file.called
+        # assert values are stored in wrapper object
+        assert wrapper.gold_standard_file == gold_standard_file
+        assert wrapper.live_annotator_name == live_annotator_name
+        assert wrapper.live_annotator_url == live_annotator_url
+        assert wrapper.language == language
+        # assert the configuration upload function was called
+        assert mock_upload_experiment_configuration.called
+        assert mock_is_url_valid.called
+    except Exception:
+        assert False
+
+
+def test_initialization_without_files_or_annotator_fails():
+    with pytest.raises(Exception):
+        GerbilExperimentApiWrapper(
+            gold_standard_file=gold_standard_file, language=language)
